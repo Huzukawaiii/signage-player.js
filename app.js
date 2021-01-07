40a1757312a8e6d509a896f0e6d9ec8b2f4e0fd5
Huzukawaiii <cowiig@gmail.com>
2021-01-07 13:00:26 +0100

    .

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const express = require("express")
-const fs = require("fs")
-const app = express()
-const port = 3000
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const port = 3000;
 
-fs.readdir("./contents/", async (err, files) => {
-    files.forEach(file => {
-        console.log(typeof file);
-        fs.readFile("./contents/" + file, async function read(err, data) {
-            await console.log(data)
-        })
-    });
-});
-
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     var result;
 
-    
+    fs.readdir("./contents/", async (err, files) => {
+        files.forEach((file) => {
+            var extension = file.substring(file.lastIndexOf(".") + 1);
 
-    res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>' + result + '</body></html>')
-})
+            if (extension == "png") {
+            }
+        });
+    });
+
+    res.send(
+        `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>${result}</body></html>`
+    );
+});
 
 app.listen(port, () => {
-    console.log(`App listening at http://127.0.0.1:${port}`)
-})
+    console.log(`App listening at http://127.0.0.1:${port}`);
+});
