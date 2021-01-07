c3a0505603ec7d5acff3947081095ed2d79a1dcc
Huzukawaiii <cowiig@gmail.com>
2021-01-07 12:11:19 +0100

    .

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ const fs = require("fs")
 const app = express()
 const port = 3000
 
+fs.readdir("./contents/", async (err, files) => {
+    files.forEach(file => {
+        console.log(typeof file);
+        fs.readFile("./contents/" + file, async function read(err, data) {
+            await console.log(data)
+        })
+    });
+});
+
 app.get('/', (req, res) => {
     var result;
 
-    fs.readdir("./contents/", (err, files) => {
-        files.forEach(file => {
-            console.log(file);
-        });
-    });
+    
 
     res.send('<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>' + result + '</body></html>')
 })
