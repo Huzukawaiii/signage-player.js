5dbaaea84ec3040210ac3ccbd18771c707edc8e1
Huzukawaiii <cowiig@gmail.com>
2021-01-07 13:58:22 +0100

    .

--- app.js
+++ app.js
@@ -11,13 +11,12 @@ app.get("/", (req, res) => {
             var extension = file.substring(file.lastIndexOf(".") + 1);
 
             if (extension == "png") {
+
             }
         });
     });
 
-    res.send(
-        `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title></head><body>${result}</body></html>`
-    );
+    res.sendFile(__dirname + "/views/index.1.html");
 });
 
 app.listen(port, () => {
